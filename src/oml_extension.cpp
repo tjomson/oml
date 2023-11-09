88e9282ed283d54ae6613a5eaeb8fde362a2d9ad
tjomson <thomaskilbak@gmail.com>
2023-11-09 12:38:17 +0100

    Make more rows

--- src/oml_extension.cpp
+++ src/oml_extension.cpp
@@ -10,14 +10,19 @@
 #include <duckdb/parser/parsed_data/create_scalar_function_info.hpp>
 
 // OpenSSL linked through vcpkg
+#include <fstream>
 #include <iostream>
 #include <openssl/opensslv.h>
 
 namespace duckdb {
 
-struct OMLData : public TableFunctionData {
-  vector<string> files;
-  idx_t filename_col_idx;
+struct OMLFunctionData : public TableFunctionData {
+  string file;
+  vector<LogicalType> return_types;
+};
+
+struct OMLData : public GlobalTableFunctionState {
+  bool finished = false;
 };
 
 //struct OMLLocalstate : public LocalTableFunctionState {
@@ -37,26 +42,72 @@ struct OMLData : public TableFunctionData {
 //  idx_t file_index;
 //};
 
+static void InitTable(vector<LogicalType> &return_types, vector<string> &return_names) {
+  return_names.emplace_back("experiment_id");
+  return_types.emplace_back(LogicalType::VARCHAR);
+  return_names.emplace_back("node_id");
+  return_types.emplace_back(LogicalType::VARCHAR);
+  return_names.emplace_back("node_id_seq");
+  return_types.emplace_back(LogicalType::VARCHAR);
+  return_names.emplace_back("time_sec");
+  return_types.emplace_back(LogicalType::VARCHAR);
+  return_names.emplace_back("time_usec");
+  return_types.emplace_back(LogicalType::VARCHAR);
+  return_names.emplace_back("power");
+  return_types.emplace_back(LogicalType::FLOAT);
+  return_names.emplace_back("current");
+  return_types.emplace_back(LogicalType::FLOAT);
+  return_names.emplace_back("voltage");
+  return_types.emplace_back(LogicalType::FLOAT);
+}
+
 static unique_ptr<FunctionData> ReadOMLBind(ClientContext &context, TableFunctionBindInput &input,
                                             vector<LogicalType> &return_types, vector<string> &return_names) {
-  auto result = make_uniq<OMLData>();
-  result->files = MultiFileReader::GetFileList(context, input.inputs[0], "OML");
-  return_types.emplace_back(LogicalType::VARCHAR);
-  return_names.emplace_back("stuff");
-//  result->return_types.emplace_back(LogicalType::VARCHAR);
-//  result->return_names.emplace_back("stuff");
-  std::cout << "bind done" << std::endl;
+  auto result = make_uniq<OMLFunctionData>();
+  result->file = StringValue::Get(input.inputs[0]);
+  InitTable(return_types, return_names);
+  std::cout << "bind done " << input.inputs[0] << std::endl;
   return std::move(result);
+}
+
+unique_ptr<GlobalTableFunctionState> ReadOMLInit(ClientContext &context, TableFunctionInitInput &input) {
+  return make_uniq<OMLData>();
 }
 
 static void ReadOMLFunction(ClientContext &context, TableFunctionInput &data_p, DataChunk &output) {
   std::cout << "start oml function" << std::endl;
-  auto &bind_data = data_p.bind_data->CastNoConst<OMLData>();
+  auto &bind_data = data_p.bind_data->CastNoConst<OMLFunctionData>();
+  auto &data = data_p.global_state->Cast<OMLData>();
+  if (data.finished) return;
+//  output.Initialize(context, bind_data.return_types, 1);
 //  auto &data = data_p.global_state->Cast<GlobalTableFunctionState>();
 //  auto &lstate = data_p.local_state->Cast<LocalTableFunctionState>();
-  std::cout << "lmao " << bind_data.files[0] << std::endl;
+//  std::cout << "lmao " << bind_data.file << std::endl;
+//  for (auto type :output.GetTypes()) {
+//    std::cout << type.ToString() << std::endl;
+//  }
+  for (int i = 0; i < 3; i++) {
+    output.SetValue(0, i, Value("hej0"+std::to_string(i)));
+    output.SetValue(1, i, Value("hej1"+std::to_string(i)));
+    output.SetValue(2, i, Value("hej2"+std::to_string(i)));
+    output.SetValue(3, i, Value("hej3"+std::to_string(i)));
+    output.SetValue(4, i, Value("hej4"+std::to_string(i)));
+    output.SetValue(5, i, Value(0.1 + i));
+    output.SetValue(6, i, Value(0.2 + i));
+    output.SetValue(7, i, Value(0.3 + i));
+  }
+  output.SetCardinality(3);
+  data.finished = true;
 
-//  std::cout << "lmao" << std::endl;
+//  std::cout << output.ToString() << std::endl;
+
+
+//  string line;
+//  std::ifstream file(bind_data.file);
+//  while (getline (file, line)) {
+////    std::cout << line << std::endl;
+//  }
+//  file.close();
 }
 
 //inline void OmlScalarFun(DataChunk &args, ExpressionState &state, Vector &result) {
@@ -83,7 +134,7 @@ static void LoadInternal(DatabaseInstance &instance) {
     // Register a scalar function
 //    auto oml_scalar_function = ScalarFunction("oml", {LogicalType::VARCHAR}, LogicalType::VARCHAR, OmlScalarFun);
 //    ExtensionUtil::RegisterFunction(instance, oml_scalar_function);
-    TableFunction read_oml("read_oml", {LogicalType::VARCHAR}, ReadOMLFunction, ReadOMLBind);
+    TableFunction read_oml("read_oml", {LogicalType::VARCHAR}, ReadOMLFunction, ReadOMLBind, ReadOMLInit);
     ExtensionUtil::RegisterFunction(instance, read_oml);
 
     // Register another scalar function
